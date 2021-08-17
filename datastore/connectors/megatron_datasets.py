7d6412fdbdb95ff6a32f92d7d18986200ed7664d
ontocord <ontocord@gmail.com>
2021-08-17 19:24:08 -0400

    Update megatron_datasets.py

--- datastore/connectors/megatron_datasets.py
+++ datastore/connectors/megatron_datasets.py
@@ -1,3 +1,7 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             os.path.pardir, os.path.pardir)))
+
 #################################################################################################################################
 # Code for megatron mmap indexed datasets, which are in turn based on memmap datasets, and a custom numpy based index file.
 
