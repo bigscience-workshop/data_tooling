d6a7b5214ede51fe7caff3f85d6ffeb03faed58a
root <ontocord@gmail.com>
2021-08-31 04:40:20 +0000

    fixing pii code

--- processors/tests/pii_tests.py
+++ processors/tests/pii_tests.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
                                              os.path.pardir, os.path.pardir, os.path.pardir)))
@@ -8,5 +9,5 @@ if __name__ == "__main__":
           """Dr. Butcher is a pediatrician at King Saud University. He is a Muslim man and his SSN is 555-50-3371.""",
           """WASHINGTON — A startup under contract to the U.S. Space Force is investigating the use of solar-powered vehicles for operations in deep space beyond Earth orbit. As the Space Force plans possible missions in cislunar space — the vast area between the Earth and the moon — one of the concerns are the limitations of traditional chemical propulsion. Spacecraft powered by solar thermal energy that use water as its main propellant could provide a viable alternative, says Shawn Usman, astrophysicist and founder of startup Rhea Space Activity. RSA won a Small Business Innovation Research (SBIR) Phase 1 study contract funded by the Space Force to design a spacecraft with a solar-thermal propulsion system."""]
   pii_manager = PIIProcessor('en')
-  for docId, line in enumerate(lines):
-    print (pii_manager.process(line, docId=docId))
+  for row_id, line in enumerate(lines):
+    print (pii_manager.process(line, row_id=row_id))
