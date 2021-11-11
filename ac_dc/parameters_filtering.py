21b67896fd69a48c7b5424424396fc9c99bd4d14
Francesco De Toni <32432020+clancyoftheoverflow@users.noreply.github.com>
2021-11-11 23:03:29 +0800

    Added parameters for "fr"

--- ac_dc/parameters_filtering.py
+++ ac_dc/parameters_filtering.py
@@ -34,7 +34,26 @@ parameters_filtering_en = {
     "lang_id_cutoff": 0.8,
 }
 
+parameters_filtering_fr = {
+    "cond_remove_words_with_incorrect_substrings": True,
+    "incorrect_word_substrings": ["http", "www", ".com", "href", "//"],
+    "cond_remove_long_words": True,
+    "length_word_cutoff": 25,
+    "cond_check_empty": True,
+    "strip_characters": "' 0123456789¯_%$§½¼¾×|†~\"—±′–'°−{}[]·'?,./<>!@#^&*()+-‑=:;，：,...�`→¶.…‘’”€«»",
+    "cond_check_special_characters": True,
+    "special_characters": "' 0123456789¯_%$§½¼¾×|†~\"—±′–'°−{}[]·'?,./<>!@#^&*()+-‑=:;，：,...�`→¶.…‘’”€«»",
+    "special_characters_cutoff": 0.4,
+    "cond_check_stopwords": True,
+    "stopwords_cutoff": 0.4,
+    "cond_check_badwords": True,
+    "badwords_cutoff": 0.4,
+    "cond_check_lang_id": True,
+    "lang_id_cutoff": 0.8,
+}
+
 parameters_filtering = {
     "default": parameters_filtering_default,
     "en": parameters_filtering_en,
+    "fr": parameters_filtering_fr,
 }
