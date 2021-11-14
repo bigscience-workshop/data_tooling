ec9e6bc50e04ecf160d8c70033ec4f2780b35412
edugp <edugp91@gmail.com>
2021-11-14 10:22:36 -0800

    Tokenize using trained sentence piece model before computing perplexity

--- perplexity_lenses/perplexity_lenses/perplexity.py
+++ perplexity_lenses/perplexity_lenses/perplexity.py
@@ -3,8 +3,23 @@ import re
 import unicodedata
 import urllib.request
 from typing import Dict
+import sentencepiece
 
 import kenlm
+
+
+class SentencePiece:
+    def __init__(
+        self,
+        model: str,
+    ):
+        super().__init__()
+        self.sp = sentencepiece.SentencePieceProcessor()
+        self.sp.load(str(model))
+
+    def do(self, text: dict) -> dict:
+        tokenized = self.sp.encode_as_pieces(text)
+        return " ".join(tokenized)
 
 
 class KenlmModel:
@@ -54,6 +69,7 @@ class KenlmModel:
         download_kenlm_model(language)
         try:
             self.model = kenlm.Model(f"{language}.arpa.bin")
+            self.tokenizer = SentencePiece(f"{language}.sp.model")
         except OSError:
             os.remove(f"{language}.arpa.bin")
             if os.path.exists(f"{language}.sp.model"):
@@ -66,16 +82,21 @@ class KenlmModel:
     def from_pretrained(cls, language: str):
         return cls(language)
 
+    def pp(self, log_score, length):
+        return 10.0 ** (-log_score / length)
+
     def get_perplexity(self, doc: str, normalize_cc_net: bool = True):
         if normalize_cc_net:
             doc = self.normalize(doc)
+        # Tokenize (after normalizing): See https://github.com/facebookresearch/cc_net/blob/bda555bd1cf1ee2e0b925363e62a61cd46c8b60d/cc_net/mine.py#L352 for full pipeline
+        doc = self.tokenizer.do(doc)
         doc_log_score, doc_length = 0, 0
         for line in doc.split("\n"):
             log_score = self.model.score(line)
             length = len(line.split()) + 1
             doc_log_score += log_score
             doc_length += length
-        return 10.0 ** (-doc_log_score / doc_length)
+        return round(self.pp(doc_log_score, doc_length), 1)
 
     def normalize(
         self,
