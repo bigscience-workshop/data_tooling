949a584bb51074d85eddb5fe86d81af3da60c239
Eduardo Gonzalez Ponferrada <edugp91@gmail.com>
2021-11-17 11:00:01 -0800

    Normalize and use sentence piece tokenizer (#76)
    * Normalize and use sentence piece tokenizer before computing perplexity for pre-trained cc_net KenLM models from Facebook
    
    * Format file

--- ac_dc/download_kenlm_models.py
+++ ac_dc/download_kenlm_models.py
@@ -17,8 +17,12 @@ def download_kenlm_models(output_path: str) -> None:
     # unsupported_kenlm_langs = langs_id.loc[~langs_id.oscar_id.isin(langs_id.kenlm_id)].oscar_id.dropna().unique()
     for lang in supported_kenlm_langs:
         try:
-            output = subprocess.check_output(
+            output_kenlm = subprocess.check_output(
                 f"wget http://dl.fbaipublicfiles.com/cc_net/lm/{lang}.arpa.bin -P {output_path}",
+                shell=True,
+            )
+            output_sentence_piece = subprocess.check_output(
+                f"wget http://dl.fbaipublicfiles.com/cc_net/lm/{lang}.sp.model -P {output_path}",
                 shell=True,
             )
         except:
