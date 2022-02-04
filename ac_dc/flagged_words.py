6540bcb3cd2dbaa3b20c8a1bd84e4cbde7ebe50b
Joseph Tobing <joseph.haposan@gmail.com>
2022-02-05 00:10:27 +0700

    Final update for Indonesian list of flagged words (#395)
    * further updates to flagged_words.py for indonesian
    
    * Removed English words in the Indonesian list as they are already included
    
    * final update for flagged_words.py
    
    * fixed some accidental omissions
    
    Co-authored-by: Joseph Tobing <joseph.h.tobing@gmail.com>
    Co-authored-by: Hugo Laurençon <44556846+HugoLaurencon@users.noreply.github.com>

--- ac_dc/flagged_words.py
+++ ac_dc/flagged_words.py
@@ -573,6 +573,9 @@ flagged_words = {
     ],
     "id": english_flagged_words
     + [
+        "bokep",
+        "coli",
+        "colmek",
         "grepe",
         "horni",
         "janda",
@@ -582,12 +585,14 @@ flagged_words = {
         "kontol",
         "masturbasi",
         "memek",
+        "ngentot",
+        "ngewe",
         "peju",
         "pepek",
         "pornografi",
+        "sange",
         "sepong",
         "tusbol",
-        "tusuk bola",
     ],
     "kn": english_flagged_words
     + [
