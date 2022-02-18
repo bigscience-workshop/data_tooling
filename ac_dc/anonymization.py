223da895eac674a5b393df32f0fb185fad0b2ba5
Ian Yu <ian.yu@arc.com.co>
2022-02-18 16:12:49 -0500

    updated anonymization
    By default, tag_type for anonymization is None to allow passing
    iterables as tag_type. This allows applying anonymization of
    specific types for specific situation. By default, if tag_type
    is None, all keys in regex_rulebase would be applied.

--- ac_dc/anonymization.py
+++ ac_dc/anonymization.py
@@ -182,14 +182,25 @@ iban_regex = {
 
 
 def apply_regex_anonymization(
-    sentence: str, lang_id: str, context_window: int = 20, anonymize_condition=None
+    sentence: str, lang_id: str, context_window: int = 20, anonymize_condition=None, tag_type=None
 ) -> str:
+    """
+    Params:
+    ==================
+    sentence: str, the sentence to be anonymized
+    lang_id: str, the language id of the sentence
+    context_window: int, the context window size
+    anonymize_condition: function, the anonymization condition
+    tag_type: iterable, the tag types of the anonymization. All keys in regex_rulebase by default
+    """
+    if tag_type == None:
+        tag_type = regex_rulebase.keys()
     lang_id = lang_id.split("_")[0]
     ner = detect_ner_with_regex_and_context(
         sentence=sentence,
         src_lang=lang_id,
         context_window=context_window,
-        tag_type=regex_rulebase.keys(),
+        tag_type=tag_type,
     )
     if anonymize_condition:
         for (ent, start, end, tag) in ner:
