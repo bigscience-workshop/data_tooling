a59089697597c6aca822bad27edd407aa54ff31f
SaulLu <55560583+SaulLu@users.noreply.github.com>
2022-02-21 17:42:24 +0100

    Apply cleaning: remove deduplicated exact lines (#408)
    * update `pseudo_crawl_seed_to_lm_dset`
    
    * temporary hack to launch
    
    * slurm script
    
    * modify yacine script
    
    * update slurm script
    
    * duplicate script
    
    * update new script
    
    * Nit
    
    * Count lines across documents instead of within
    
    * Minimum of characters per line
    
    * Export skipped lines in another file
    
    * Update slurm script
    
    * Remove none text
    
    * Woops
    
    * Fit Thomas setup
    
    * Woops
    
    * Woops
    
    * Woops
    
    * Woops
    
    * Woops
    
    * Have a save batch size
    
    * Deactivate old code
    
    * Woops added more cores
    
    * Woops added more cores
    
    * Debug why np.choice fails
    
    * I'm stupid
    
    * I'm stupid
    
    * I'm stupid
    
    * I'm stupid
    
    * Remove columns as the dataset changes size
    
    * Woops
    
    * Lower memory footprint
    
    * Got OOM
    
    * Add simple log
    
    * Change strategy to move folders after
    
    * Woops
    
    * Fix caching
    
    * Try lowering memory footprint by 20
    
    * Remove columns before map to reduce memory footprint
    
    * Reduce the number of processes
    
    * Ask for maximum time
    
    * Use list comprehention to make it faster
    
    * Revert "Use list comprehention to make it faster"
    
    This reverts commit 5ccc341799422a987ce9a1f938651f4df6c18ab3.
    
    * Hopefully this helps
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: thomasw21 <24695242+thomasw21@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- cc_pseudo_crawl/python_scripts/load_all_seed_ids.py
+++ cc_pseudo_crawl/python_scripts/load_all_seed_ids.py
@@ -1,13 +1,12 @@
 import csv
 from argparse import ArgumentParser
-from pathlib import Path
 
 
 def get_args():
     parser = ArgumentParser()
     parser.add_argument(
-        "--seed-path",
-        type=str,
+        "--seed-paths",
+        type=lambda x: x.split(","),
         required=True,
         help="Seed full path. e.g. 'xxx/seeds.csv'",
     )
@@ -20,11 +19,13 @@ def get_args():
 def main():
     args = get_args()
 
-    with open(args.seed_path, "r") as fi:
-        data = csv.reader(fi)
-        # First line is all the headers that we remove.
-        seed_ids = [row[0] for row_id, row in enumerate(data) if row_id > 0]
-        print(seed_ids[args.seed_index])
+    seed_ids = []
+    for seed_path in args.seed_paths:
+        with open(seed_path, "r") as fi:
+            data = csv.reader(fi)
+            # First line is all the headers that we remove.
+            seed_ids += [row[0] for row_id, row in enumerate(data) if row_id > 0]
+    print(seed_ids[args.seed_index])
 
 
 if __name__ == "__main__":
